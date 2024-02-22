bc2bfb396d5f8d97a830673d6402e47b0b5951b8
김명혜 <imbrightsun1@gmail.com>
2024-02-22 21:55:45 +0900

    Starbucks2_배치 숨김 보임 처리 (lodash & gsap 라이브러리 활용)

--- 13_starbucks/js/main2.js
+++ 13_starbucks/js/main2.js
@@ -1,3 +1,5 @@
+// 클릭, 블러 시 인풋박스 포커스 처리
+
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
@@ -13,3 +15,30 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+
+
+// 스크롤에 따라 배지 숨김/보임 처리
+
+const badgeEl = document.querySelector('header .badges');
+// 부하 방지 (스크롤 작업 시 많이 사용됨)
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    //배지숨김
+    // badgeEl.style.display = 'none';
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    //배지보임
+  //   badgeEl.style.display = 'block';
+  gsap.to(badgeEl, .6, {
+    opacity: 1,
+    display: 'block'
+  });
+  }
+}, 300));
+// _.throttle(함수,시간)
