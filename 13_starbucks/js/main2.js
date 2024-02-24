e0793df5402b6ae8d5ad903c9893b1ea20b25725
김명혜 <imbrightsun1@gmail.com>
2024-02-24 14:33:47 +0900

    Starbucks2_section_visual (순차적으로 요소 보이기: JS_gsap활용)

--- 13_starbucks/js/main2.js
+++ 13_starbucks/js/main2.js
@@ -1,5 +1,4 @@
-// 클릭, 블러 시 인풋박스 포커스 처리
-
+// 클릭, 블러 시 인풋박스 포커스 처리 ------------------------------------
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
@@ -17,9 +16,7 @@ searchInputEl.addEventListener('blur', function () {
 });
 
 
-
-// 스크롤에 따라 배지 숨김/보임 처리
-
+// 스크롤에 따라 배지 숨김/보임 처리 ------------------------------------
 const badgeEl = document.querySelector('header .badges');
 // 부하 방지 (스크롤 작업 시 많이 사용됨)
 window.addEventListener('scroll', _.throttle(function () {
@@ -42,3 +39,13 @@ window.addEventListener('scroll', _.throttle(function () {
   }
 }, 300));
 // _.throttle(함수,시간)
+
+
+//비쥬얼에 있는 요소들 순차적으로 나타내기 --------------------------------
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7, //0.7초 후, 1.4초 후, 2.1초 후, 2.8초 후 나타남
+    opacity: 1
+  });
+});
