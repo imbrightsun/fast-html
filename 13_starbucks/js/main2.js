5c920f70086f4628e687f51ded9ad099bc57e109
김명혜 <imbrightsun1@gmail.com>
2024-02-25 23:43:24 +0900

    Starbucks2_section NOTICE  (Swiper & Toggle)

--- 13_starbucks/js/main2.js
+++ 13_starbucks/js/main2.js
@@ -49,3 +49,43 @@ fadeEls.forEach(function (fadeEl, index) {
     opacity: 1
   });
 });
+
+
+//슬라이드 라이브러리 -----------------------------------------------
+new Swiper('.notice-line .swiper', {
+  // Optional parameters
+  direction: 'vertical', 
+  loop: true,
+  autoplay: true
+});
+new Swiper('.promotion .swiper', {
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  // autoplay: {
+  //   delay: 5000 // 5초
+  // },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+
+//토글 ----------------------------------------------------------
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion
+  if (isHidePromotion) {
+    promotionEl.classList.add('hide');
+  } else {
+    promotionEl.classList.remove('hide');
+  }
+});
